41534f5a3b02c4c88ab14f9cc9599177297164f8
Lorenzo Fontana <fontanalorenzo@me.com>
2013-11-13 19:57:24 +0100

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,14 @@
+package main.java;
+
+/**
+ * Class Application
+ */
+public class Application {
+    /**
+     * Main Class
+     * @param args Arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("I'm an empty Application ;) ");
+    }
+}
